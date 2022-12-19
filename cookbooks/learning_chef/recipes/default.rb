2ab0349c84ef61dea82425047644e45e72d3149e
mohammed <col.mohammedtaher@gmail.com>
2022-12-19 18:28:24 +0000

    added 'learning_chef' cookbook

---
+++ cookbooks/learning_chef/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: learning_chef
+# Recipe:: default
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
---
+++ cookbooks/learning_chef/recipes/install_package.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: learning_chef
+# Recipe:: install_package
+#
+# Copyright:: 2022, The Authors, All Rights Reserved.
+
+package 'tree' do
+  action :install
+end
